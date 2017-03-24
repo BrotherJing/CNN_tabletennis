0d98d453eb96814a134b4d25e359c60d297122f9
Brotherjing <brotherjing@sjtu.edu.cn>
2017-03-24 14:38:40 +0800

    remove unused file, edit readme

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # CNN_tablentennis
 
 Implement with the idea in SO-DLT([Transferring Rich Feature Hierarchies for Robust Visual Tracking](https://arxiv.org/abs/1501.04587))
+And also Faster R-CNN([Faster R-CNN: Towards Real-Time Object Detection with Region Proposal Networks](https://arxiv.org/abs/1506.01497))
 
 # Screenshots
 
+See more detail in the jupyter notebook :)
+
 outputs probability map
 
-![probability map](http://7xrcar.com1.z0.glb.clouddn.com/sodlt.png)
+![probability map](http://7xrcar.com1.z0.glb.clouddn.com/prob_map.png)
 
-get bounding box
+regress a bounding box
 
-![bounding box](http://7xrcar.com1.z0.glb.clouddn.com/localization.png)
+![bounding box](http://7xrcar.com1.z0.glb.clouddn.com/regression.png)
+
+tracking
+
+![tracking](http://7xrcar.com1.z0.glb.clouddn.com/tracking.png)
--- scripts/create_dataset.sh
+++ scripts/create_dataset.sh
@@ -6,14 +6,21 @@ CAFFE_ROOT=/home/jing/caffe/build_cmake
 
 echo "Creating data lmdb..."
 
-$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/filename.txt $OUTPUT_DIR/train_data_lmdb
-$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/filename_test.txt $OUTPUT_DIR/val_data_lmdb
+$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/train.txt $OUTPUT_DIR/train_data_lmdb
+$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/val.txt $OUTPUT_DIR/val_data_lmdb
+
+echo "Creating probability map lmdb..."
+
+$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/train.label.txt $OUTPUT_DIR/train_label_lmdb --gray=true
+$CAFFE_ROOT/tools/convert_imageset / $FILENAME_DIR/val.label.txt $OUTPUT_DIR/val_label_lmdb --gray=true
+
+echo "Creating bounding box label lmdb..."
+
+python scripts/gen_label.py --file=$FILENAME_DIR/train.bbox.txt --outname=$OUTPUT_DIR/train_bbox_lmdb
+python scripts/gen_label.py --file=$FILENAME_DIR/val.bbox.txt --outname=$OUTPUT_DIR/val_bbox_lmdb
 
 echo "Computing image mean..."
 
 $CAFFE_ROOT/tools/compute_image_mean $OUTPUT_DIR/train_data_lmdb $OUTPUT_DIR/mean.binaryproto
 
 python scripts/gen_py_mean.py
-
-python scripts/gen_label.py --file=$FILENAME_DIR/label.txt --outname=$OUTPUT_DIR/train_label_lmdb
-python scripts/gen_label.py --file=$FILENAME_DIR/label_test.txt --outname=$OUTPUT_DIR/val_label_lmdb
